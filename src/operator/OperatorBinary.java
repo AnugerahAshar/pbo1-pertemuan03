670af21fb94701ce0e5ef1d47627a8ba4b1fc31d
Anugerah Ashar <hutanalaska382@gmail.com>
2023-05-10 16:19:04 +0800

    Penambahan Operator Unary

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -4,4 +4,13 @@ public class OperatorBinary {
     String nama = "Anugerah Ashar";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.73;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
